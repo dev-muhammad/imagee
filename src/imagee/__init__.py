09b99feb73c967bb818fd5b8fe4ec88f0983c5bf
Muhammad <63408454+dev-muhammad@users.noreply.github.com>
2021-09-05 09:40:02 +0500

    refactoring with OOP

--- src/imagee/__init__.py
+++ src/imagee/__init__.py
@@ -9,6 +9,47 @@ import os.path
 import sys
 from PIL import Image
 
+class Imagee():
+    """Main class"""
+    image = None
+    size = None
+    format = None
+    path = None
+    optimized_size = None
+    optimization_rate = None
+    quality = 85
+    optimized_image = None
+
+    def read(self, path):
+        """Read image from path"""
+        self.format = _validateImage(path)
+        self.size = _checkSize(path)
+        self.image = Image.open(path)
+        self.path = path 
+        pass
+
+    def optimaze(self, quality=85):
+        """
+        Optimaze method for image optimization
+        """
+        self.image.save(
+                        self.optimized_image, 
+                        self.format, 
+                        optimize=True, 
+                        quality=self.quality)
+        file_size_after = self.optimized_image.getbuffer().nbytes # get image size after optimizing
+        self.optimization_rate = round((self.size-file_size_after)/float(self.size), 2)
+        pass
+
+    def save(self, path):
+        """Save image on path"""
+        # self.optimized_image.save(buffer, file_type, optimize=True, quality=quality)
+        pass
+
+    def getBase64():
+        """Return image in base64 format"""
+        pass
+
 def optimize(inputPath, outputPath, quality=85):
     """
     Optimaze method for image optimizing
@@ -55,7 +96,7 @@ def _validateImage(path):
     if os.path.exists(path):
         _, file_extension = os.path.splitext(path)
         if file_extension in SUPPORTED_FORMATS:
-            return None
+            return file_extension
         else:
             sys.exit(f'Format {file_extension} not supported! Use {SUPPORTED_FORMATS}')
     else:
